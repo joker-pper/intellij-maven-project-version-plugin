b54651999e3d6f44121c558e123ad029505f2a3c
yyc <417004794@qq.com>
2021-04-25 13:43:56 +0800

    update show version...

--- src/main/java/com/github/jokerpper/mavenprojectversion/handler/ShowMavenProjectVersionHandler.java
+++ src/main/java/com/github/jokerpper/mavenprojectversion/handler/ShowMavenProjectVersionHandler.java
@@ -7,27 +7,14 @@ import com.github.jokerpper.mavenprojectversion.util.StringUtils;
 import com.intellij.ide.projectView.PresentationData;
 import com.intellij.ide.projectView.ProjectView;
 import com.intellij.ide.projectView.ProjectViewNode;
-import com.intellij.ide.util.treeView.PresentableNodeDescriptor;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.SimpleTextAttributes;
-import com.intellij.ui.treeStructure.PatchedDefaultMutableTreeNode;
-import com.intellij.ui.treeStructure.SimpleTree;
-import com.intellij.util.ReflectionUtil;
 import org.jetbrains.idea.maven.navigator.MavenProjectsNavigator;
-import org.jetbrains.idea.maven.navigator.MavenProjectsStructure;
 import org.jetbrains.idea.maven.project.MavenProject;
 import org.jetbrains.idea.maven.project.MavenProjectsManager;
 
-import javax.swing.tree.DefaultTreeModel;
-import java.lang.reflect.Field;
-import java.util.Enumeration;
-import java.util.List;
-
 public class ShowMavenProjectVersionHandler {
-
-    private static final Logger LOG = Logger.getInstance(ShowMavenProjectVersionHandler.class);
 
     public static final ShowMavenProjectVersionHandler INSTANCE = new ShowMavenProjectVersionHandler();
 
@@ -87,49 +74,6 @@ public class ShowMavenProjectVersionHandler {
             }
         }
 
-        if (true) {
-            return;
-        }
-
-        PatchedDefaultMutableTreeNode patchedDefaultMutableTreeNode = getPatchedDefaultMutableTreeNode(mavenProjectsNavigator);
-        if (patchedDefaultMutableTreeNode == null) {
-            return;
-        }
-
-        Enumeration patchedDefaultMutableTreeNodeChildren = patchedDefaultMutableTreeNode.children();
-        while (patchedDefaultMutableTreeNodeChildren != null && patchedDefaultMutableTreeNodeChildren.hasMoreElements()) {
-            Object child = patchedDefaultMutableTreeNodeChildren.nextElement();
-            Field userObjectField = ReflectionUtil.getDeclaredField(child.getClass(), "userObject");
-            Object userObjectTemp = ReflectionUtil.getFieldValue(userObjectField, child);
-            MavenProjectsStructure.ProjectNode userObject;
-            if (userObjectTemp instanceof MavenProjectsStructure.ProjectNode) {
-                userObject = (MavenProjectsStructure.ProjectNode) userObjectTemp;
-                String version = StringUtils.trim(userObject.getMavenProject().getMavenId().getVersion());
-                PresentationData presentationData = userObject.getPresentation();
-                String formatVersion = String.format("\t %s", version);
-                boolean hasFormatVersion = false;
-                int hasFormatVersionIndex = -1;
-
-                List<PresentableNodeDescriptor.ColoredFragment> coloredFragmentList = presentationData.getColoredText();
-                for (PresentableNodeDescriptor.ColoredFragment coloredFragment : coloredFragmentList) {
-                    hasFormatVersionIndex++;
-                    if (StringUtils.equals(coloredFragment.getText(), formatVersion)) {
-                        hasFormatVersion = true;
-                        break;
-                    }
-                }
-
-                if (!hasFormatVersion) {
-                    if (isShow) {
-                        addColoredText(presentationData, formatVersion, SimpleTextAttributes.GRAY_SMALL_ATTRIBUTES);
-                    }
-                } else {
-                    if (!isShow) {
-                        presentationData.getColoredText().remove(hasFormatVersionIndex);
-                    }
-                }
-            }
-        }
     }
 
     public void refreshMavenProjectView(Project project) {
@@ -157,56 +101,6 @@ public class ShowMavenProjectVersionHandler {
             return false;
         }
         return true;
-    }
-
-    /**
-     * get simple tree
-     *
-     * @param mavenProjectsNavigator
-     * @return
-     */
-    private SimpleTree getSimpleTree(MavenProjectsNavigator mavenProjectsNavigator) {
-        try {
-            Field myTreeField = ReflectionUtil.getDeclaredField(MavenProjectsNavigator.class, "myTree");
-            if (myTreeField == null) {
-                return null;
-            }
-            SimpleTree simpleTree = (SimpleTree) ReflectionUtil.getFieldValue(myTreeField, mavenProjectsNavigator);
-            if (simpleTree == null) {
-                return null;
-            }
-            return simpleTree;
-        } catch (Exception e) {
-            LOG.warn("get simple tree error", e);
-        }
-        return null;
-    }
-
-    /**
-     * get patched default mutable tree node
-     *
-     * @param mavenProjectsNavigator
-     * @return
-     */
-    private PatchedDefaultMutableTreeNode getPatchedDefaultMutableTreeNode(MavenProjectsNavigator mavenProjectsNavigator) {
-        try {
-            SimpleTree simpleTree = getSimpleTree(mavenProjectsNavigator);
-            if (simpleTree == null) {
-                return null;
-            }
-            Field treeModelField = ReflectionUtil.getDeclaredField(SimpleTree.class, "treeModel");
-            if (treeModelField == null) {
-                return null;
-            }
-            DefaultTreeModel defaultTreeModel = ReflectionUtil.getFieldValue(treeModelField, simpleTree);
-            if (defaultTreeModel == null) {
-                return null;
-            }
-            return (PatchedDefaultMutableTreeNode) defaultTreeModel.getRoot();
-        } catch (Exception e) {
-            LOG.warn("get patched default mutable tree node error", e);
-        }
-        return null;
     }
 
     private void addColoredText(PresentationData data, String text) {
--- src/main/java/com/github/jokerpper/mavenprojectversion/startup/MavenProjectVersionStartupActivity.java
+++ src/main/java/com/github/jokerpper/mavenprojectversion/startup/MavenProjectVersionStartupActivity.java
@@ -5,34 +5,20 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.project.ProjectManagerListener;
 import com.intellij.openapi.startup.StartupActivity;
-import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.openapi.wm.ToolWindowManager;
-import com.intellij.openapi.wm.ex.ToolWindowManagerListener;
 import com.intellij.util.messages.MessageBusConnection;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.idea.maven.navigator.MavenProjectsNavigator;
 
 public class MavenProjectVersionStartupActivity implements StartupActivity {
 
     @Override
     public void runActivity(@NotNull Project project) {
+        ShowMavenProjectVersionHandler.INSTANCE.syncIsShowStructureView(project);
 
         MessageBusConnection connection = project.getMessageBus().connect();
-
         connection.subscribe(ProjectManager.TOPIC, new ProjectManagerListener() {
             @Override
             public void projectClosingBeforeSave(@NotNull Project project) {
                 ShowMavenProjectVersionHandler.INSTANCE.syncIsShowStructureView(project);
-            }
-        });
-
-        connection.subscribe(ToolWindowManagerListener.TOPIC, new ToolWindowManagerListener() {
-            @Override
-            public void stateChanged(@NotNull ToolWindowManager toolWindowManager) {
-                ToolWindow toolWindow = toolWindowManager.getToolWindow(MavenProjectsNavigator.TOOL_WINDOW_ID);
-                if (toolWindow != null) {
-                    ShowMavenProjectVersionHandler.INSTANCE.syncIsShowStructureView(project);
-                }
             }
         });
 
